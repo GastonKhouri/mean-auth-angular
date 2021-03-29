a5e5c7a93118cd9c4b885598e1ca164519b82ef8
GastonKhouri <gastonlozano2016@yahoo.com>
2021-03-28 20:11:12 -0430

    JWT, rutas y autenticacion listos

---
+++ README.md
@@ -0,0 +1,17 @@
+# MEAN auth backend
+
+Se realizó un backend para ser implementado con angular, se está realizando utilizando el MEAN stack (Mongo, Express, Angular, Node).
+
+El backend implementa lo siguiente:
+
+- REST Services
+- JWT
+- MongoDB - Mongo Atlas
+- Express framework
+- Express validator
+- CRUD
+- Validaciones
+- Modelos de base de datos
+- Encriptar contraseñas
+
+Recuerden instalar los modulos de node con `npm install`.
---
+++ controllers/auth.js
@@ -0,0 +1,121 @@
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+
+const { generarJWT } = require('../helpers/jwt');
+const Usuario = require('../models/Usuario');
+
+const crearUsuario = async(req, res = response) => {
+
+	const { email, name, password } = req.body;
+
+	try {
+
+		// Verificar el email
+		const usuario = await Usuario.findOne({email});
+
+		if(usuario) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Ya existe un usuario con ese email'
+			})
+		}
+
+		// Crear usuario con el modelo
+		const dbUser = new Usuario(req.body);
+
+		// Hashear la contraseña
+		const salt = bcrypt.genSaltSync();
+		dbUser.password = bcrypt.hashSync(password, salt);
+
+		// Generar JWT
+		const token = await generarJWT(dbUser.id, name);
+
+		// Crear usuario de DB
+		await dbUser.save();
+
+		// Generar respuesta exitosa
+		return res.status(201).json({
+			ok: true,
+			uid: dbUser.id,
+			name,
+			token
+		});
+
+
+	} catch(error) {
+		console.log(error);
+		return res.status(500).json({
+				ok: false,
+				msg: 'Por favor, hable con el administrador'
+			});
+	}
+
+}
+
+const loginUsuario = async(req, res) => {
+
+	const { email, password } = req.body; 
+
+	try {
+
+		const dbUser = await Usuario.findOne({email});
+
+		if(!dbUser) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'El correo no existe'
+			})
+		}
+
+		// Confirmar si password hace match
+		const validPassword = bcrypt.compareSync(password, dbUser.password);
+
+		if(!validPassword) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'El password es incorrecto'
+			});
+		}
+
+		// Generar el JWT
+		const token = await generarJWT(dbUser.id, dbUser.name);
+
+		return res.json({
+			ok: true,
+			uid: dbUser.id,
+			name: dbUser.name,
+			token
+		});
+
+	} catch(error) {
+		console.log(error);
+		return res.status(500).json({
+			ok: false,
+			msg: 'Por favor, hable con el administrador'
+		});
+	}
+
+}
+
+const revalidarToken = async(req, res) => {
+
+	const { uid, name } = req;
+
+	// Generar el JWT
+	const token = await generarJWT(uid, name);
+
+	return res.json({
+		ok: true,
+		uid,
+		name,
+		token
+	});
+	
+}
+
+
+module.exports = {
+	crearUsuario,
+	loginUsuario,
+	revalidarToken
+}
